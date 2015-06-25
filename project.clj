37b04fc0f5159acff4c26de3073045a8136cbc2d
Eric Shull <shull@atomicobject.com>
2015-06-25 07:23:11 -0400

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject vdom-app/lein-template "0.1.0"
+  :description "Lein template for creating vdom-based ClojureScript applications."
+  :url "http://github.com/exupero/vdom-app"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :eval-in-leiningen true)
---
+++ src/leiningen/new/vdom_app.clj
@@ -0,0 +1,20 @@
+(ns leiningen.new.vdom-app
+  (:use [leiningen.new.templates :only [renderer name-to-path ->files]]))
+
+(def render (renderer "vdom-app"))
+
+(defn vdom-app [name]
+  (let [data {:name name
+              :path (name-to-path name)}]
+    (->files data
+      ["index.html" (render "index.html" data)]
+      ["dev.html" (render "dev.html" data)]
+      ["project.clj" (render "project.clj" data)]
+      "externs"
+      ["externs/vdom.js" (render "externs/vdom.js" data)]
+      "src/{{path}}"
+      ["src/{{path}}/core.cljs" (render "src/vdom_app/core.cljs" data)]
+      "resources/public/js"
+      ["resources/public/js/vdom.js" (render "resources/public/js/vdom.js" data)]
+      "resources/public/css"
+      "resources/public/css/{{path}}.css" (render "resources/public/css/styles.css" data))))
