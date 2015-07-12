6acd9fae251bcf469d489db12752aaafde876531
Eric Shull <shull@atomicobject.com>
2015-07-11 22:14:51 -0400

    No need for go macro.

--- src/leiningen/new/vdom_app/src/vdom_app/core.cljs
+++ src/leiningen/new/vdom_app/src/vdom_app/core.cljs
@@ -1,5 +1,4 @@
 (ns {{name}}.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs.core.async :as async :refer [chan]]
             [cljs.core.match :refer-macros [match]]
             [vdom.elm :refer [foldp render!]]))
@@ -14,8 +13,7 @@
   (match action
     :no-op model))
 
-(go
-  (let [actions (chan)
-        initial-model {}
-        models (foldp step initial-model actions)]
-    (render! (async/map (ui actions) [models]) js/document.body)))
+(let [actions (chan)
+      initial-model {}
+      models (foldp step initial-model actions)]
+  (render! (async/map (ui actions) [models]) js/document.body))
