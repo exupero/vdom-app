3f49f0c298ab3cf9e2fab9e651ef081a4b786cbf
Eric Shull <eric@exupero.org>
2015-10-23 09:37:28 -0400

    Remove extraneous space.

--- src/leiningen/new/vdom_app/project.clj
+++ src/leiningen/new/vdom_app/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122" :exclusions [org.apache.ant/ant]]
-                 [org.clojure/core.match  "0.3.0-alpha4"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [vdom "0.1.1-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "0.3.2"]
